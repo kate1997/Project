b2b6b39a1713a0d2146af26c45a88d86857a1198
Katya Ipatova <kate1997ipatova2@yandex.ru>
2019-01-15 17:51:13 +0300

    add providedAdd and index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+function check(){
+
+    var name = document.getElementById('name').value;
+    var cost = document.getElementById('cost').value;
+    var cost_par = /[0-9]+/;
+    var provCost = cost_par.test(cost);
+    var raiting = document.getElementById('raiting').value;
+    var time_par = /[0-9]+/;
+    var provRaiting = time_par.test(raiting);
+    
+    
+    if( name == "" && cost =="" && raiting==""){
+       alert('Заполните поля!');
+    }
+    else{
+       if( name == "" || cost =="" || raiting==""){
+          alert('Вы не заполнили одно из полей');
+       }
+          else{
+             if(provCost == true && provRaiting ==true){
+                 alert('Данные успешно отправлены');
+             }
+                else alert('Поля должны содержать только числовые значения');    
+          }     
+    }
+    
+    }
